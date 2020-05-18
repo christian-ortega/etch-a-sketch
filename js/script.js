90e8c431fadece2bf4b8dd62821f54c7fa6ab675
Christian Ortega <christianmortega7@gmail.com>
2020-05-18 13:17:31 -0400

    Implement default shading on hover (black)

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@ let gridElement;
 for(let i = 0; i < 256; i++) {
     gridElement = document.createElement("div");
     gridElement.classList.toggle("grid-element");
+    gridElement.addEventListener("mouseover", (e) => {
+        e.target.classList.add("colored");
+    });
     gridContainer.appendChild(gridElement);
 }
